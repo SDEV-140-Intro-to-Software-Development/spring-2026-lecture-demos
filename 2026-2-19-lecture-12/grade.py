613ec7e7a67c8145038b229447454030cb38acb5
Eric Sexton <esexton15@ivytech.edu>
2026-02-19 14:47:03 -0500

    examples

---
+++ 2026-2-19-lecture-12/grade.py
@@ -0,0 +1,29 @@
+"""
+Student Grade Tracker
+
+Provides core library functions for creating grade tracking software.
+
+Functions:
+    caclulate_averages(grades): calculates the average
+"""
+
+
+def caclulate_averages(grades):
+    """
+    Calculates the average grade from a list of grades (floats)
+
+    Args:
+        grades: list of floats between 0 and 100
+
+    Returns:
+        Average grade as float
+
+    Raises:
+        ValueError: if grade list is empty
+    """
+
+    print("Hello World")
+
+
+
+avg = caclulate_averages()
---
+++ 2026-2-19-lecture-12/main.py
@@ -0,0 +1,3 @@
+import grade
+
+grade.caclulate_averages()
